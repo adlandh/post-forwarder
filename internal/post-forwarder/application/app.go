1ec16c9af62fc86e67d86873708be2a56b3389f6
Adlan DH <adlandh@gmail.com>
2023-10-13 13:24:30 +0200

    markdown -> html (#14)

--- internal/post-forwarder/application/app.go
+++ internal/post-forwarder/application/app.go
@@ -3,11 +3,14 @@ package application
 import (
 	"context"
 	"fmt"
+	"regexp"
 
 	"github.com/adlandh/post-forwarder/internal/post-forwarder/domain"
 )
 
 const MaxMessageLength = 4_096
+
+var r = regexp.MustCompile(`<.*?>`)
 
 var _ domain.ApplicationInterface = (*Application)(nil)
 
@@ -24,13 +27,15 @@ func NewApplication(notifier domain.Notifier) *Application {
 func (a Application) ProcessRequest(ctx context.Context, service string, msg string) error {
 	subject := genSubject(service)
 
+	msg = r.ReplaceAllString(msg, "")
+
 	msg = limitMessageLength(subject, msg)
 
 	return a.notifier.Send(ctx, subject, msg)
 }
 
 func genSubject(service string) string {
-	return fmt.Sprintf("Message from *%s*:", service)
+	return fmt.Sprintf("Message from <b>%s</b>:", service)
 }
 
 func limitMessageLength(subject, msg string) string {
