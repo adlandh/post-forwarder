cfb36763e906d87e575c403877935973d5943bc6
DH <adlandh@gmail.com>
2024-10-21 17:34:07 +0200

    update mockery config

--- internal/post-forwarder/domain/interfaces.go
+++ internal/post-forwarder/domain/interfaces.go
@@ -1,4 +1,6 @@
 // Package domain contains application domain layer
+//
+//go:generate mockery
 package domain
 
 import (
@@ -7,23 +9,20 @@ import (
 	"time"
 )
 
-//go:generate mockery --name ApplicationInterface
-//go:generate gowrap gen -p github.com/adlandh/post-forwarder/internal/post-forwarder/domain -i ApplicationInterface -t https://raw.githubusercontent.com/adlandh/gowrap-templates/main/sentry.gotmpl -o ./wrappers/ApplicationInterfaceWithSentry.go -l "" -g
+//go:generate gowrap gen -i ApplicationInterface -t https://raw.githubusercontent.com/adlandh/gowrap-templates/main/sentry.gotmpl -o ./wrappers/ApplicationInterfaceWithSentry.go -l "" -g
 type ApplicationInterface interface {
 	ProcessRequest(ctx context.Context, url string, service string, msg string) (err error)
 	GetMessage(ctx context.Context, id string) (msg string, createdAt time.Time, err error)
 }
 
-//go:generate mockery --name Notifier
-//go:generate gowrap gen -p github.com/adlandh/post-forwarder/internal/post-forwarder/domain -i MessageStorage -t https://raw.githubusercontent.com/adlandh/gowrap-templates/main/sentry.gotmpl -o ./wrappers/MessageStorageWithSentry.go -l "" -g
+//go:generate gowrap gen -i MessageStorage -t https://raw.githubusercontent.com/adlandh/gowrap-templates/main/sentry.gotmpl -o ./wrappers/MessageStorageWithSentry.go -l "" -g
 type Notifier interface {
 	Send(ctx context.Context, service, msg string) (err error)
 }
 
 var ErrorNotFound = fmt.Errorf("not found")
 
-//go:generate mockery --name MessageStorage
-//go:generate gowrap gen -p github.com/adlandh/post-forwarder/internal/post-forwarder/domain -i Notifier -t https://raw.githubusercontent.com/adlandh/gowrap-templates/main/sentry.gotmpl -o ./wrappers/NotifierWithSentry.go -l "" -g
+//go:generate gowrap gen -i Notifier -t https://raw.githubusercontent.com/adlandh/gowrap-templates/main/sentry.gotmpl -o ./wrappers/NotifierWithSentry.go -l "" -g
 type MessageStorage interface {
 	Store(ctx context.Context, msg string) (id string, err error)
 	Read(ctx context.Context, id string) (msg string, createdAt time.Time, err error)
