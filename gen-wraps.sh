f327c55bc98f5666794649889fcb2e46169feecb
DH <adlandh@gmail.com>
2025-06-02 10:15:14 +0200

    Update dependencies and improve script quoting for interface wrapping

--- gen-wraps.sh
+++ gen-wraps.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for i in `go tool find-interfaces -path .`
+for i in $(go tool find-interfaces -path .)
 do
-  go tool gowrap gen -i $i -t https://raw.githubusercontent.com/adlandh/gowrap-templates/main/sentry.gotmpl -o ./wrappers/${i}WithSentry.gen.go -l "" -g
+  go tool gowrap gen -i "$i" -t https://raw.githubusercontent.com/adlandh/gowrap-templates/main/sentry.gotmpl -o "./wrappers/${i}WithSentry.gen.go" -l "" -g
 done
